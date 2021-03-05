6d81372461872790862680a95bd86788174b143d
Julia <77528734+juliagronau@users.noreply.github.com>
2021-03-05 14:49:46 +0100

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import './App.css';
 import {useState, useEffect} from 'react';
 import Map from './Components/Map';
+import { Container } from 'semantic-ui-react';
 
 function App() {
 let url = 'https://geo.ipify.org/api/v1?apiKey=at_w8b2xFtEcMlHvyvoYFKB50tT6p9X0';
@@ -24,12 +25,13 @@ useEffect(() => {
 }, []);
  
   return (
-    <div>
+    <Container>
+      <h1>Wanna know your IP-Address?</h1>
       <p>Your IP-Address is {ip}</p>
       <div>
       {userData ? <Map userData={userData} /> : 'Loading...'} 
       </div> 
-    </div>
+    </Container>
   );
 }
 
